9c1ddf5e99e111a87c18d5b5c0f01bd04b9fd597
seanj29 <seanotto29@gmail.com>
2023-09-03 14:34:13 +0100

    Set up FileSystemWatcher

--- Coding Puzzle/Program.cs
+++ Coding Puzzle/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace HelloWorld
+﻿using System.IO;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using var watcher = new FileSystemWatcher(@"C:\Users\seano\OneDrive\Documents\Coding\C Sharp\temp");
+
+             watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+
+            watcher.Changed += OnChanged;
+            watcher.Created += OnCreated;
+
+            watcher.Filter = "";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();                   
+        }
+         private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+         private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath}";
+            Console.WriteLine(value);
         }
     }
 }
